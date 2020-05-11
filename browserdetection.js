97a7bf71f1aadbeb47f756681b16476757a52297
hubcalifornia <miguel.gaona@hubinternational.com>
2020-05-11 14:10:18 -0700

    remove IE swapout

--- browserdetection.js
+++ browserdetection.js
@@ -30,9 +30,12 @@ function detectMobile() {
 var msg = document.getElementById("msg");
 
 
-if(detectBrowser() != "Not IE" || detectMobile()){
-    //document.getElementById("container").innerHTML = "<div id='msg'><img src='HUB-Vertical-Full-Colour-RGB_lr.jpg' id='hublogo'><p>This page is not viewable in your current browser. Please open this page in one of the supported desktop browsers below. Our apologies for the inconvenience.</p><ul><li><a href='https://www.google.com/chrome/browser/desktop/'>Chrome</a></li><li><a href='https://www.mozilla.org/en-US/firefox/52.0/releasenotes/'>Firefox 64 bit</a></li><li><a href='https://www.microsoft.com/en-us/windows/microsoft-edge'>Edge</a></li><li><a href='https://support.apple.com/downloads/safari'>Safari</a></li></ul></div>";
+if(detectMobile()){
     document.getElementById("container").innerHTML = "<iframe width='760px' height='500px' src='https://sway.office.com/s/obEqE7mN7GHftVr1/embed' frameborder='0' marginheight='0' marginwidth='0' max-width='100%' sandbox='allow-forms allow-modals allow-orientation-lock allow-popups allow-same-origin allow-scripts' scrolling='no' style='border: none; max-width: 100%; max-height: 100vh' allowfullscreen mozallowfullscreen msallowfullscreen webkitallowfullscreen></iframe>";
+}
+else if(detectBrowser() != "Not IE"){
+    document.getElementById("container").innerHTML = "<div id='msg'><img src='HUB-Vertical-Full-Colour-RGB_lr.jpg' id='hublogo'><p>This page is not viewable in your current browser. Please open this page in one of the supported desktop browsers below. Our apologies for the inconvenience.</p><ul><li><a href='https://www.google.com/chrome/browser/desktop/'>Chrome</a></li><li><a href='https://www.mozilla.org/en-US/firefox/52.0/releasenotes/'>Firefox 64 bit</a></li><li><a href='https://www.microsoft.com/en-us/windows/microsoft-edge'>Edge</a></li><li><a href='https://support.apple.com/downloads/safari'>Safari</a></li></ul></div>";
+
 }
 else{
     document.getElementById("container").innerHTML = "<iframe src='https://prezi.com/p/gf9ieqssrexe/embed/' id='iframe_container' frameborder='0' webkitallowfullscreen='' mozallowfullscreen='' allowfullscreen='' allow='autoplay; fullscreen'></iframe>"
